6aa137b54f676347eb5a3d2a1c3ba73f19321fbf
Greg Ives <gregives774@gmail.com>
2020-11-06 00:51:41 +0000

    Format files and replace processed attributes

--- index.js
+++ index.js
@@ -1,120 +1,123 @@
-const { JSDOM } = require('jsdom')
+const { JSDOM } = require("jsdom");
 
 const ATTRIBUTES = [
   {
-    tag: 'audio',
-    attribute: 'src'
+    tag: "audio",
+    attribute: "src",
   },
   {
-    tag: 'embed',
-    attribute: 'src'
+    tag: "embed",
+    attribute: "src",
   },
   {
-    tag: 'img',
-    attribute: 'src'
+    tag: "img",
+    attribute: "src",
   },
   {
-    tag: 'img',
-    attribute: 'srcset'
+    tag: "img",
+    attribute: "srcset",
   },
   {
-    tag: 'input',
-    attribute: 'src'
+    tag: "input",
+    attribute: "src",
   },
   {
-    tag: 'link',
-    attribute: 'href'
+    tag: "link",
+    attribute: "href",
   },
   {
-    tag: 'object',
-    attribute: 'data'
+    tag: "object",
+    attribute: "data",
   },
   {
-    tag: 'script',
-    attribute: 'src'
+    tag: "script",
+    attribute: "src",
   },
   {
-    tag: 'script',
-    attribute: 'href'
+    tag: "script",
+    attribute: "href",
   },
   {
-    tag: 'script',
-    attribute: 'xlink:href'
+    tag: "script",
+    attribute: "xlink:href",
   },
   {
-    tag: 'source',
-    attribute: 'src'
+    tag: "source",
+    attribute: "src",
   },
   {
-    tag: 'source',
-    attribute: 'srcset'
+    tag: "source",
+    attribute: "srcset",
   },
   {
-    tag: 'track',
-    attribute: 'src'
+    tag: "track",
+    attribute: "src",
   },
   {
-    tag: 'video',
-    attribute: 'poster'
+    tag: "video",
+    attribute: "poster",
   },
   {
-    tag: 'video',
-    attribute: 'src'
+    tag: "video",
+    attribute: "src",
   },
   {
-    tag: 'image',
-    attribute: 'xlink:href'
+    tag: "image",
+    attribute: "xlink:href",
   },
   {
-    tag: 'image',
-    attribute: 'href'
+    tag: "image",
+    attribute: "href",
   },
   {
-    tag: 'use',
-    attribute: 'xlink:href'
+    tag: "use",
+    attribute: "xlink:href",
   },
   {
-    tag: 'use',
-    attribute: 'href'
-  }
-]
+    tag: "use",
+    attribute: "href",
+  },
+];
 
 // Regex to match sources in srcset
-const SRCSET = /(?:^|,)\s*(\S+)/g
+const SRCSET = /(?:^|,)\s*(\S+)/g;
 
 // Get dependencies from attribute of element
 async function getDependencies(element, attribute) {
   // Get source from attribute of element
-  const source = element.getAttribute(attribute.attribute)
+  let source = element.getAttribute(attribute);
 
   // Parse srcset
-  const paths = attribute.attribute === 'srcset'
-    ? [...source.matchAll(SRCSET)].map(([, path]) => path)
-    : [source]
+  const paths =
+    attribute === "srcset"
+      ? [...source.matchAll(SRCSET)].map(([, path]) => path)
+      : [source];
 
   // Replace each path with the processed dependency
   for (const path of paths) {
-    source.replace(path, await this.addDependency(path))
+    source = source.replace(path, await this.addDependency(path));
   }
+
+  // Set the processed source attribute
+  element.setAttribute(attribute, source);
 }
 
-module.exports = async function(content, options) {
+module.exports = async function (content, options) {
   // Build DOM of the content
-  const dom = new JSDOM(content)
-  const { document } = dom.window
+  const dom = new JSDOM(content);
+  const { document } = dom.window;
 
   // Find all dependencies from attributes
-  for (const attribute of ATTRIBUTES) {
-    const elements = [...document.querySelectorAll(attribute.tag)]
+  for (const { tag, attribute } of ATTRIBUTES) {
+    const elements = [...document.querySelectorAll(tag)];
 
     for (const element of elements) {
-      if (!element.hasAttribute(attribute.attribute))
-        continue
+      if (!element.hasAttribute(attribute)) continue;
 
       // Get dependencies from element
-      await getDependencies.bind(this)(element, attribute)
+      await getDependencies.bind(this)(element, attribute);
     }
   }
 
-  return dom.serialize()
-}
+  return dom.serialize();
+};
